f4e3e6c5f56b45592203d1128e61301fe2b5bf11
PReichetanz <paul.reichetanz@gmx.de>
2023-03-14 15:15:47 +0100

    initial commit

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,26 +1,19 @@
-import { useEffect, useState } from "react";
-
-function useFetch(url) {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(url);
-      const newData = await response.json();
-
-      setData(newData);
-    }
-
-    startFetching();
-  }, [url]);
-
-  return data;
-}
+import { useState } from "react";
+import useSWR from "swr";
 
 export default function Joke() {
   const [id, setId] = useState(0);
 
-  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  const [jokesInfo, setJokesInfo] = useState([]);
+
+  const info = jokesInfo.find((info) => info.id === id) ?? { isFunny: false };
+
+  // destructuring of info object
+  const { isFunny } = info;
+
+  const { data, isLoading } = useSWR(
+    `https://example-apis.vercel.app/api/bad-jokes/${id}`
+  );
 
   function handlePrevJoke() {
     setId(data.prevId);
@@ -30,14 +23,40 @@ export default function Joke() {
     setId(data.nextId);
   }
 
-  if (!data) {
+  function handleToggleFunny(id) {
+    setJokesInfo((jokesInfo) => {
+      const info = jokesInfo.find((info) => info.id === id);
+
+      if (info) {
+        return jokesInfo.map((info) =>
+          info.id === id ? { ...info, isFunny: !info.isFunny } : info
+        );
+      }
+      return [...jokesInfo, { id, isFunny: true }];
+    });
+  }
+
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   return (
     <>
       <small>ID: {id}</small>
-      <h1>{data.joke}</h1>
+      <h1>
+        {data.joke}{" "}
+        <span
+          role="img"
+          aria-label={isFunny ? "A laughing face" : "An unamused face"}
+        >
+          {isFunny ? "🤣" : "😒"}
+        </span>
+      </h1>
+      <div>
+        <button type="button" onClick={() => handleToggleFunny(id)}>
+          {isFunny ? "Stop laughing" : "Start laughing"}
+        </button>
+      </div>
       <div>
         <button type="button" onClick={handlePrevJoke}>
           ← Prev Joke
